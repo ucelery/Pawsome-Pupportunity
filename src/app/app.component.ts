091799505c819c57496b4cc775d7f4d95d380e39
don <donaldolivergavino@gmail.com>
2023-10-25 15:54:44 +0800

    Integrate Homepage with the Backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Dog } from './model/dog';
+import { DogService } from './service/dog.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pawsome-pupportunity';
+  dogs: Dog [] = []
+  constructor(private dogService: DogService) { }
+
+  ngOnInit(): void {
+    this.dogService.getDogs().subscribe((data: Dog[]) => {
+      this.dogs = data;
+    });
+    
+  }
 }
