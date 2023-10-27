a145b630887c0e0bd339cf056b0a43597bedfe4a
don <donaldolivergavino@gmail.com>
2023-10-28 00:00:55 +0800

    Integrate View to Adopt Page

--- src/app/adopt/adopt.component.ts
+++ src/app/adopt/adopt.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Dog } from '../model/dog';
 import { DogService } from '../service/dog.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-adopt',
@@ -9,12 +10,21 @@ import { DogService } from '../service/dog.service';
 })
 export class AdoptComponent {
   dogs: Dog[] = [];
-  constructor(private dogService: DogService) {}
+  constructor(private dogService: DogService,
+    private router: Router) {}
 
   ngOnInit(): void {
 	this.dogService.getDogs().subscribe((data: Dog[]) => {
       this.dogs = data; console.log(this.dogs);
     });
 
+  }
+
+  toView(dogId : number){
+    // You can define the route path where you want to navigate to
+    const routePath = `/view/${dogId}`;
+
+    // Navigate to the specified route
+    this.router.navigate([routePath]);
   }
 }
