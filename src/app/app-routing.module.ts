1b0c7d30fc680c3902028ef17e02c21c007eedce
nokori <69152061+ucelery@users.noreply.github.com>
2023-10-27 20:13:31 +0800

    Fix Merge Conflicts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< Updated upstream
 import { HomeComponent } from './home/home.component';
 import { AdoptComponent } from './adopt/adopt.component';
 
@@ -8,12 +7,6 @@ const routes: Routes = [
   {path:'', redirectTo:'home', pathMatch:'full'},
   {path:'home', component: HomeComponent},
   {path:'adopt', component: AdoptComponent}
-=======
-import { AdoptComponent } from './adopt/adopt.component';
-
-const routes: Routes = [
-  {path: 'adopt', component: AdoptComponent},
->>>>>>> Stashed changes
 ];
 
 @NgModule({
