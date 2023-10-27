578cdcdcc26bf88a594697227223318309cafdb5
nokori <69152061+ucelery@users.noreply.github.com>
2023-10-27 22:37:54 +0800

    Finalize Popup Description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Dog } from './model/dog';
 import { DogService } from './service/dog.service';
 
@@ -8,14 +8,40 @@ import { DogService } from './service/dog.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  @ViewChild('DogDeetsPopup') DogDeetsPopup: ElementRef;
+
+  // Description Modal Things
+  @ViewChild('DogName') DogName!: ElementRef;
+  @ViewChild('DogAge') DogAge!: ElementRef;
+  @ViewChild('DogSex') DogSex!: ElementRef;
+  @ViewChild('DogSize') DogSize!: ElementRef;
+  @ViewChild('DogBreed') DogBreed!: ElementRef;
+  @ViewChild('DogBirthday') DogBirthday!: ElementRef;
+  @ViewChild('DogDescription') DogDescription!: ElementRef;
+
   title = 'pawsome-pupportunity';
-  dogs: Dog [] = []
-  constructor(private dogService: DogService) { }
+  dogs: Dog [] = []  
+  visible_pop_up = false
+
+  constructor(private dogService: DogService) { 
+    this.DogDeetsPopup = new ElementRef(null)
+  }
 
   ngOnInit(): void {
     this.dogService.getDogs().subscribe((data: Dog[]) => {
       this.dogs = data;
-    });
-    
+    }); 
+  }
+
+  toggleDogDescription(id: number) {
+    if (this.visible_pop_up)  {
+      // HIDE
+      this.DogDeetsPopup.nativeElement.classList.add('hide');
+      this.visible_pop_up = false
+    } else {
+      // SHOW
+      this.DogDeetsPopup.nativeElement.classList.remove('hide');
+      this.visible_pop_up = true
+    }
   }
 }
