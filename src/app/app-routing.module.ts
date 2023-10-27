83459f765a9c851564f6403fbf3121775b391020
rae <miyanalistella7@gmail.com>
2023-10-27 20:06:02 +0800

    Create Popup View

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+<<<<<<< Updated upstream
 import { HomeComponent } from './home/home.component';
 import { AdoptComponent } from './adopt/adopt.component';
 
@@ -7,6 +8,12 @@ const routes: Routes = [
   {path:'', redirectTo:'home', pathMatch:'full'},
   {path:'home', component: HomeComponent},
   {path:'adopt', component: AdoptComponent}
+=======
+import { AdoptComponent } from './adopt/adopt.component';
+
+const routes: Routes = [
+  {path: 'adopt', component: AdoptComponent},
+>>>>>>> Stashed changes
 ];
 
 @NgModule({
